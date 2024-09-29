dde0011177cc57f4297b9b70e4b276594a104d1a
DEEPAK P <deepakram1774@gmail.com>
2024-09-29 19:52:38 +0530

    Add files via upload

---
+++ DAY 1/swap.java
@@ -0,0 +1,12 @@
+public class swap {
+    public static void main(String[] args) 
+    {
+        int x=10;
+        int y=20;
+        x=x+y;
+        y=x-y;
+        x=x-y;
+        System.out.println("x:"+x);
+        System.out.println("y:"+y);
+    }
+}
